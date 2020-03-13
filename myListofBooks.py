628fecbdd1df52e7c9ff54c37a4dc58c2dd04d5a
NattheCh <nataliachamerska@gmail.com>
2020-03-13 21:31:35 +0100

    Adding sorting list of books to myListofBooks

--- myListofBooks.py
+++ myListofBooks.py
@@ -2,20 +2,17 @@ import os.path
 
 booksTitles = []
 if os.path.isfile('file.txt'):
-    textFile = open('file.txt', 'r+')
-    print('The books which are currently added to the library:')
-    print(textFile.read())
-else:
-    textFile = open('file.txt', 'w')
+    booksTitles = open('file.txt').read().splitlines()
+    for title in booksTitles:
+        print(title)
+textFile = open('file.txt', 'w+')
 while True:
-    print('Name of the book is (To end do not enter anything.):')
+    print('\nName of the book is (To end do not enter anything.):')
     title = input()
     if title == '':
         break
     booksTitles = booksTitles + [title]
-print('The books added to my home library: ')
-for title in booksTitles:
-    print(title)
+booksTitles = sorted(booksTitles)
 for title in booksTitles:
     textFile.write(str(title) + '\n')
 textFile.close()
