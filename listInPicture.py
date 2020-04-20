06281f34b918a232e862f48c1e4f381b3006fc61
NattheCh <nataliachamerska@gmail.com>
2020-04-20 13:03:14 +0200

    Exercise with matrix

--- listInPicture.py
+++ listInPicture.py
@@ -12,4 +12,4 @@ grid = [['.', '.', '.', '.', '.', '.'],
 for x in range(len(grid[0])):
         for y in range(len(grid)):
                 print(grid[y][x], end='')
-        print('\n')
+        print()
