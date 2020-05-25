090658f2e0c512eb332fd836d56ce0ef7537eeb2
NattheCh <nataliachamerska@gmail.com>
2020-05-25 22:07:41 +0200

    appGUI update

--- appGUI.py
+++ appGUI.py
@@ -2,13 +2,21 @@ from tkinter import *
 
 window = Tk()
 
+
 window.title('Colour word')
 window.geometry('200x150')
-label = Label(window, text='Enter the word from colours'.center(50))
+label = Label(window, text='Enter the color - blue'.center(50))
 label.grid(column=0, row=0)
-txt = Entry(window, width=20)
-txt.grid(column=0, row=1)
-pressButton = Button(window, text='Confirm')
+txtColor = Entry(window, width=20)
+txtColor.grid(column=0, row=1)
+def clicked():
+    if txtColor.get() == 'blue':
+        label.configure(text='Button checked'.center(50))
+    else:
+        label.configure(text='Not correct'.center(50))
+
+
+pressButton = Button(window, text='Confirm', command=clicked)
 pressButton.grid(column=0, row=2)
 
 window.mainloop()
